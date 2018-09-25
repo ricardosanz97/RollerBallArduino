febcba71e7f6b6f95ca5d41751bbd46662a99bde
Ricardo <ricardosanz97contact@gmail.com>
2018-09-25 17:12:50 +0200

    añadido el proyecto de Unity

---
+++ RollerBall/Assets/_Completed-Game/Scripts/ArduinoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoController : MonoBehaviour
+{
+	private SerialPort stream;
+	private string ping = "PING";
+	private string pong = "PONG";
+	
+	void Start () {
+		
+        stream = new SerialPort("COM9", 9600);
+        stream.ReadTimeout = 50;
+        stream.Open();
+        //stream.WriteLine(ping);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		print(ReadFromArduino());
+	}
+
+	public string ReadFromArduino(int timeout = 1)
+	{
+		stream.ReadTimeout = timeout;
+		try
+		{
+			string streamReadline = stream.ReadLine();
+			print(streamReadline);
+			return streamReadline;
+
+		}
+		catch (TimeoutException e)
+		{
+			return null;
+		}
+	}
+}
